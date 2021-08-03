0d13f49530a1c42513ce6504398fb6bbf29be940
Chamari Jayanthimala <chamarijayanthimala@gmail.com>
2021-08-03 11:48:33 +0530

    test

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const myfirstelement = <h1>Hello React!</h1>
- class Car {
-   constructor(name){
-      this.brand = name;
-   }
- }
 
-const mycar = new Car("Audi");
 ReactDOM.render(myfirstelement, document.getElementById('root'));
